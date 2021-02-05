72b567ebe0416fc568b4b7ad4cc0e0c953ec8ef0
marcosdam <marcosledesmapicazo@gmail.com>
2021-02-05 15:49:46 +0100

    Initial Commit

---
+++ Ejemplo05-CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejemplo05-CoreData
+//
+//  Created by Marcos Ledesma on 05/02/2021.
+//  Copyright © 2021 Marcos Ledesma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
